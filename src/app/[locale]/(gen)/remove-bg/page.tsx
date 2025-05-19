2a07bc1bf19f31ea7d7cfb865e15aa99823e1f31
nietao <nietao2018@gmail.com>
2025-05-19 22:08:17 +0800

    feat: bugfix

--- src/app/[locale]/(gen)/remove-bg/page.tsx
+++ src/app/[locale]/(gen)/remove-bg/page.tsx
@@ -1,10 +1,27 @@
 import { unstable_setRequestLocale } from 'next-intl/server';
 import ImageUploaderClient from './ImageUploaderClient';
+import HeroLanding from '@/components/sections/hero-landing';
+import PreviewLanding from '@/components/sections/preview-landing';
+import StepsSection from '@/components/sections/steps';
+import BentoGrid from '@/components/sections/bentogrid';
+import InfoLanding from '@/components/sections/info-landing';
+import Features from '@/components/sections/features';
+import Testimonials from '@/components/sections/testimonials';
+import CTA from '@/components/sections/CTA';
 
 export default function RemoveBgPage({ params }: { params: { locale: string } }) {
   // 在服务器组件中设置语言环境
   unstable_setRequestLocale(params.locale);
-  
+
   // 渲染客户端组件
-  return <ImageUploaderClient />;
+  return <>
+    <ImageUploaderClient />
+    {/* <Powered /> */}
+    <StepsSection />
+    <BentoGrid />
+    <InfoLanding />
+    <Features />
+    <Testimonials />
+    <CTA />
+  </>;
 }
